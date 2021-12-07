0c8ce6aec63f74f133b4fb0a2bdb660dc63927cc
Lefucjusz <elektromarcin@gmail.com>
2021-12-07 21:30:29 +0100

    Added procfile, added heroku port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const api = require('./api');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
