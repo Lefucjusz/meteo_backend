362762d601c115c1680671a2f97c469b63ec1e2d
Lefucjusz <elektromarcin@gmail.com>
2022-05-21 22:07:07 +0200

    Added endpoint to select by name
    Added API endpoint to select measurements
    by sensor name. Changed webpage to get
    only measurements from Quark platform

--- src/api/measurements.js
+++ src/api/measurements.js
@@ -13,4 +13,20 @@ router.get('/getAll', async (req, res) => {
     }
 });
 
+router.get('/getByName/:name', async (req, res) => {
+    const requestedSensorName = req.params.name;
+    console.log(`[INFO]: [GET] /api/measurements/getByName/${requestedSensorName}`);
+    try {
+        const data = await db.measurement.findAll({
+            where: {
+                sensorName: requestedSensorName
+            }
+        });
+        res.send(data);
+    } catch(err) {
+        console.log(`[ERROR]: [GET] /api/measurements/getByName/${requestedSensorName}: ${err}`);
+        res.status(500).send('Database error!');
+    }
+});
+
 module.exports = router;
