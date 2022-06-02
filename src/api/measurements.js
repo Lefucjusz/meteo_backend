9a33bcdc00dd4254457ec63f49abdca2c8be183b
Lefucjusz <elektromarcin@gmail.com>
2022-06-02 21:01:54 +0200

    Reverted query change, values ignored while plotted
    Reverted previous change of the query
    as it didn't work as it was planned
    Added ignoring errorneous humidity
    measurements while the plots are
    being created on the webpage.

--- src/api/measurements.js
+++ src/api/measurements.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const { Op } = require('sequelize');
 
 router.get('/getAll', async (req, res) => {
     console.log('[INFO]: [GET] /api/measurements/getAll');
@@ -18,13 +17,9 @@ router.get('/getByName/:name', async (req, res) => {
     const requestedSensorName = req.params.name;
     console.log(`[INFO]: [GET] /api/measurements/getByName/${requestedSensorName}`);
     try {
-        const maxHumidity = 100; // 100%
         const data = await db.measurement.findAll({
             where: {
-                sensorName: requestedSensorName,
-                humidity: {
-                    [Op.lte]: maxHumidity // Remove errorneous measurements
-                }
+                sensorName: requestedSensorName
             }
         });
         res.send(data);
